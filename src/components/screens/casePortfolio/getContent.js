b6883a5757d49ebf04b4154155327f115b01065a
zadfiya <narenzadfiya3579@gmail.com>
2022-08-23 14:09:47 +0530

    remove warnings

--- src/components/screens/casePortfolio/getContent.js
+++ src/components/screens/casePortfolio/getContent.js
@@ -1,23 +1,23 @@
-// /* eslint-disable import/prefer-default-export */
-// import { CMSGraphQLClient, gql } from '../../../infra/cms/CMSGraphQLClient';
+/* eslint-disable import/prefer-default-export 
+import { CMSGraphQLClient, gql } from '../../../infra/cms/CMSGraphQLClient';
 
-// export async function getContent() {
-//   const query = gql`
-//     {
-//         allPageCasePortfolios {
-//         id
-//         title
-//         description
-//         ref {
-//             description
-//         }
-//         }
-//     }
-//   `;
+export async function getContent() {
+  const query = gql`
+    {
+        allPageCasePortfolios {
+        id
+        title
+        description
+        ref {
+            description
+        }
+        }
+    }
+  `;
 
-//   const client = CMSGraphQLClient();
+  const client = CMSGraphQLClient();
 
-//   const response = await client.query({ query });
+  const response = await client.query({ query });
 
-//   return response.data.messages;
-// }
+  return response.data.messages;
+} */
