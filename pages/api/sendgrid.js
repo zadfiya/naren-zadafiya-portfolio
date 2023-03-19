6a8de2018e4386f78137a7c51cc3dfdb75aeb82f
Naren Zadafiya <narenzadfiya3579@gmail.com>
2023-03-18 20:28:32 -0400

    deployment issue

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -1,5 +1,5 @@
 import sendgrid from '@sendgrid/mail';
-//import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 const API_KEY = process.env.SENDGRID_API_KEY;
 sendgrid.setApiKey(API_KEY);
 
@@ -15,8 +15,6 @@ sendgrid.setApiKey(API_KEY);
 
 // send mail with defined transport object
 
-
-
 // async function sendEmail(req, res) {
 //   try {
 //     // console.log('REQ.BODY', req.body);
@@ -24,7 +22,7 @@ sendgrid.setApiKey(API_KEY);
 //       from: "narendrazadafiya.albetrios@gmail.com" , // sender address
 //       to: "narenzadafiya.cn@gmail.com", // list of receivers
 //       subject: `email of: ${req.body.name}`, // Subject line
-//       text: `🙋/Name: ${req.body.name}\n ✉️Email: ${req.body.email}\n 📝Mensagem: ${req.body.message}`, // plain text body
+//       text: `🙋/Name: ${req.body.name}\n ✉️Email: ${req.body.email}\n 📝Mensagem: ${req.body.message}`,
 //       html: "<b>Hello world?</b>", // html body
 //     }).then(info=>console.log(info.messageId)).catch(error=>console.log(error));
 //   } catch (error) {
@@ -38,8 +36,8 @@ async function sendEmail(req, res) {
   try {
     // console.log('REQ.BODY', req.body);
     await sendgrid.send({
-      from: "narendrazadafiya.albetrios@gmail.com" , // sender address
-      to: "narenzadafiya.cn@gmail.com", // list of receivers
+      from: 'narendrazadafiya.albetrios@gmail.com' , // sender address
+      to: 'narenzadafiya.cn@gmail.com', // list of receivers
       // to: 'canovember01@gmail.com', // Your email where you'll receive emails
       // from: 'canovember01@gmail.com', // your website email address here
       subject: `email de: ${req.body.name}`,
