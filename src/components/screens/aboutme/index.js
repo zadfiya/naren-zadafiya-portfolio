bfd41c3a38bb9a23007b56991768dbe550052e72
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-07-05 16:32:25 -0400

    About Me Screen Added

---
+++ src/components/screens/aboutme/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from '../../foundation/box';
+import Text from '../../foundation/text';
+import ContainerSobre, {
+Description, Channels, Image, Info
+} from './style';
+
+const AboutMe = () =>{
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default AboutMe;
